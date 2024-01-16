3666b74cf4f79ba80c7b6b5745b742f26b2ecd2c
korridor <26689068+korridor@users.noreply.github.com>
2024-01-16 22:39:28 +0100

    Added filament

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\Filament\AdminPanelProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
