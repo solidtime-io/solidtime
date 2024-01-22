1fe3f9836b53b5ed2daafcdbdbe7e561df2f1cf1
Gregor Vostrak <gregorvostrak@Gregors-MacBook-Pro.local>
2024-01-22 18:24:02 +0100

    remove additional waitForUrl in fixture

--- playwright/fixtures.ts
+++ playwright/fixtures.ts
@@ -49,11 +49,6 @@ export const test = baseTest.extend<object, { workerStorageState: string }>({
                 .fill('amazingpassword123');
             await page.getByRole('button', { name: 'Register' }).click();
 
-            // Wait until the page receives the cookies.
-            //
-            // Sometimes login flow sets cookies in the process of several redirects.
-            // Wait for the final URL to ensure that the cookies are actually set.
-            await page.waitForURL(PLAYWRIGHT_BASE_URL + '/dashboard');
             // Alternatively, you can wait until the page reaches a state where all cookies are set.
             await expect(
                 page.getByRole('heading', { name: 'Dashboard' })
