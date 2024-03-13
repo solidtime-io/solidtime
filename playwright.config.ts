a2aa891bbcd9a2c66a58ee75ac7f365093aadda0
Gregor Vostrak <me@onatcer.com>
2024-03-13 17:12:27 +0100

    turn trace on for all tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
         // baseURL: 'http://127.0.0.1:3000',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-        trace: process.env.CI ? 'on-first-retry' : 'on',
+        trace: 'on',
     },
 
     timeout: 10 * 1000,
