9c216cd1c7422099b8c1937368f7a72107560cf6
Gregor Vostrak <gregorvostrak@Gregors-MacBook-Pro.local>
2024-03-13 17:38:32 +0100

    update dependencies

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
         // baseURL: 'http://127.0.0.1:3000',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-        trace: 'on',
+        trace: process.env.CI ? 'on-first-retry' : 'on',
     },
 
     timeout: 10 * 1000,
