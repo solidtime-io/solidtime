b76da91cd7ddef5c1a43d809bbd5560efb18b9a3
korridor <26689068+korridor@users.noreply.github.com>
2024-03-19 17:19:54 +0100

    Added timezone for user; Moved dashboard to controller

---
+++ app/Service/TimezoneService.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use Carbon\CarbonTimeZone;
+use DateTimeZone;
+
+class TimezoneService
+{
+    /**
+     * @return array<string>
+     */
+    public function getTimezones(): array
+    {
+        $tzlist = CarbonTimeZone::listIdentifiers(DateTimeZone::ALL);
+
+        return $tzlist;
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    public function getSelectOptions(): array
+    {
+        $tzlist = $this->getTimezones();
+        $options = [];
+        foreach ($tzlist as $tz) {
+            $options[$tz] = $tz;
+        }
+
+        return $options;
+    }
+}
