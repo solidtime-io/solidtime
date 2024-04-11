6e2d0739efdc483b0dc37fddbde5072b79c3bfb5
Constantin Graf <constantin@solidtime.io>
2024-04-11 13:00:09 +0200

    Added private build

--- config/app.php
+++ config/app.php
@@ -169,13 +169,12 @@ return [
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\Filament\AdminPanelProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
+        // Warning: Do not add TelescopeServiceProvider here since it is already conditionally registered in AppServiceProvider
     ])->toArray(),
 
     /*
