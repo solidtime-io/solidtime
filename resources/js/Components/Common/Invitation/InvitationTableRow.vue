aaba46ce4f124da43a7574923d3c515abcd04ff9
Gregor Vostrak <gregorvostrak@Gregors-MacBook-Pro.local>
2024-04-15 23:45:27 +0200

    add invitations tab to members page

---
+++ resources/js/Components/Common/Invitation/InvitationTableRow.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { Invitation } from '@/utils/api';
+import TableRow from '@/Components/TableRow.vue';
+import { capitalizeFirstLetter } from '../../../utils/format';
+
+defineProps<{
+    invitation: Invitation;
+}>();
+</script>
+
+<template>
+    <TableRow>
+        <div class="whitespace-nowrap px-3 py-4 text-sm text-muted">
+            {{ invitation.email }}
+        </div>
+        <div class="whitespace-nowrap px-3 py-4 text-sm text-muted">
+            {{ capitalizeFirstLetter(invitation.role) }}
+        </div>
+    </TableRow>
+</template>
+
+<style scoped></style>
