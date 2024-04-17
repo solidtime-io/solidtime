7b05ec00548fa42d585f172117bdfddbb3a2e3ec
Gregor Vostrak <gregor@solidtime.io>
2024-04-17 17:33:02 +0200

    add project color selector

---
+++ resources/js/Components/Common/Project/ProjectColorSelector.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import Dropdown from '@/Components/Dropdown.vue';
+import { colors } from '@/utils/color';
+
+const model = defineModel<string>({ default: '' });
+</script>
+
+<template>
+    <div class="px-3">
+        <Dropdown align="bottom">
+            <template #trigger>
+                <div
+                    :style="{
+                        backgroundColor: model,
+                        boxShadow: `var(--tw-ring-inset) 0 0 0 calc(5px + var(--tw-ring-offset-width)) ${model}30`,
+                    }"
+                    class="w-4 h-4 rounded-full cursor-pointer"></div>
+            </template>
+            <template #content>
+                <div class="text-white grid grid-cols-6 gap-3 px-3 py-3">
+                    <div
+                        v-for="color in colors"
+                        :key="color"
+                        @click="model = color"
+                        :style="{
+                            backgroundColor: color,
+                            boxShadow: `var(--tw-ring-inset) 0 0 0 calc(3px + var(--tw-ring-offset-width)) ${color}30`,
+                        }"
+                        class="w-4 h-4 rounded-full cursor-pointer"></div>
+                </div>
+            </template>
+        </Dropdown>
+    </div>
+</template>
+
+<style scoped></style>
