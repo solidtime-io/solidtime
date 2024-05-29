bc8a2ce005b60fe13da8e865e5e4ec168041e6cb
Constantin Graf <constantin@solidtime.io>
2024-05-29 15:44:47 +0200

    Updated dependencies; Added widgets and charts

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Providers\Filament;
 
+use App\Filament\Widgets\ActiveUserOverview;
+use App\Filament\Widgets\TimeEntriesCreated;
+use App\Filament\Widgets\UserRegistrations;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -12,7 +15,6 @@ use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
-use Filament\Widgets;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -41,7 +43,9 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
+                ActiveUserOverview::class,
+                UserRegistrations::class,
+                TimeEntriesCreated::class,
             ])
             ->plugins([
                 EnvironmentIndicatorPlugin::make()
