62ac23cb1afccbd6963c2a8340ecade65424a7ab
Constantin Graf <constantin@solidtime.io>
2024-07-01 12:08:53 +0200

    Fixed tests after adding schema dumps for test database

--- tests/Unit/Database/MigrationTest.php
+++ tests/Unit/Database/MigrationTest.php
@@ -5,23 +5,15 @@ declare(strict_types=1);
 namespace Tests\Unit\Database;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Artisan;
+use Tests\TestCase;
 
-class MigrationTest extends \Tests\TestCase
+class MigrationTest extends TestCase
 {
     use RefreshDatabase;
 
     public function test_fresh_migration_and_rollback_runs_successfully(): void
     {
-        Artisan::call('migrate:fresh');
-        Artisan::call('migrate:rollback');
-        $this->assertTrue(true);
-    }
-
-    public function testFreshMigrationWithSeederAndRollbackRunsSuccessfully(): void
-    {
-        Artisan::call('migrate:fresh --seed');
-        Artisan::call('migrate:rollback');
-        $this->assertTrue(true);
+        $this->artisan('migrate:rollback')
+            ->assertSuccessful();
     }
 }
--- tests/Unit/Database/SeederTest.php
+++ tests/Unit/Database/SeederTest.php
@@ -5,23 +5,25 @@ declare(strict_types=1);
 namespace Tests\Unit\Database;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Artisan;
+use Tests\TestCase;
 
-class SeederTest extends \Tests\TestCase
+class SeederTest extends TestCase
 {
     use RefreshDatabase;
 
     public function test_running_the_seeder_multiple_times_runs_successfully(): void
     {
-        Artisan::call('db:seed');
-        Artisan::call('db:seed');
-        $this->assertTrue(true);
+        $this->artisan('db:seed')
+            ->assertSuccessful();
+        $this->artisan('db:seed')
+            ->assertSuccessful();
     }
 
     public function test_fresh_migration_with_seeder_and_rollback_runs_successfully(): void
     {
-        Artisan::call('migrate:fresh --seed');
-        Artisan::call('migrate:rollback');
-        $this->assertTrue(true);
+        $this->artisan('db:seed')
+            ->assertSuccessful();
+        $this->artisan('migrate:rollback')
+            ->assertSuccessful();
     }
 }
