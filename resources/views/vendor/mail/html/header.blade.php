055d93f7a3c8a4546cc3307b59332665f91e2242
Constantin Graf <constantin@solidtime.io>
2024-07-18 12:37:13 +0200

    Published mail layout and added logo

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,12 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+@if(trim($slot) === 'solidtime')
+<img src="{{ asset('images/solidtime-logo.svg') }}" class="logo" alt="solidtime Logo">
+@else
+{{ $slot }}
+@endif
+</a>
+</td>
+</tr>
