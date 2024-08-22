1328692faf726e7dbe66afb832ccbdbf0e270e34
Gregor Vostrak <gregor@solidtime.io>
2024-08-22 15:39:04 +0200

    fix ui exports, change api package bunder to vite, fix type exports

---
+++ resources/js/packages/api/vite.config.js
@@ -0,0 +1,16 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+    plugins: [dts()],
+    build: {
+        lib: {
+            // Could also be a dictionary or array of multiple entry points
+            entry: resolve(__dirname, 'src/index.ts'),
+            name: 'SolidtimeApi',
+            // the proper extensions will be added
+            fileName: 'solidtime-api',
+        },
+    },
+})
