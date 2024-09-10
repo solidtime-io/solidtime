85315fc62f0a7c1374abc139aeec7b4cea80f0b6
Gregor Vostrak <gregor@solidtime.io>
2024-09-10 18:46:35 +0200

    add client grouping and expandable project tasks to project task timetracker dropdown, fixes ST-253

--- resources/js/packages/ui/src/Project/ProjectDropdownItem.vue
+++ resources/js/packages/ui/src/Project/ProjectDropdownItem.vue
@@ -8,11 +8,14 @@ defineProps<{
 
 <template>
     <div
-        class="flex items-center space-x-3 w-full px-3 py-2.5 text-start text-sm font-medium leading-5 text-white hover:bg-card-background-active focus:outline-none focus:bg-card-background-active transition duration-150 ease-in-out">
-        <div
-            :style="{ backgroundColor: color }"
-            class="w-3 h-3 rounded-full"></div>
-        <span>{{ name }}</span>
+        class="flex justify-between items-center w-full text-start text-sm font-medium leading-5 text-white hover:bg-card-background-active focus:outline-none focus:bg-card-background-active transition duration-150 ease-in-out">
+        <div class="flex space-x-3 items-center px-3 py-1.5">
+            <div
+                :style="{ backgroundColor: color }"
+                class="w-3 h-3 rounded-full"></div>
+            <span>{{ name }}</span>
+        </div>
+        <slot name="actions"></slot>
     </div>
 </template>
 
