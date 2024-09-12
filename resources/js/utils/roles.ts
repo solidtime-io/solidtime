9189910136f873e270fcbdbbf2fa44424aa1338c
Gregor Vostrak <gregor@solidtime.io>
2024-09-12 14:41:23 +0200

    fix available roles filter, fixes ST-425

--- resources/js/utils/roles.ts
+++ resources/js/utils/roles.ts
@@ -2,6 +2,6 @@ import type { Role } from '@/types/jetstream';
 
 export function filterRoles(roles: Role[]) {
     return roles.filter(function (role) {
-        return role.key !== 'placeholder' || role.name !== 'owner';
+        return role.key !== 'placeholder' && role.key !== 'owner';
     });
 }
