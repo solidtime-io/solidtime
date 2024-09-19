a882ec6ca0c4380a4b79b9604d48c89991366a68
Constantin Graf <constantin@solidtime.io>
2024-09-19 23:49:50 +0200

    Add skip and meta to resource in time entry endpoint

--- app/Http/Resources/V1/TimeEntry/TimeEntryCollection.php
+++ app/Http/Resources/V1/TimeEntry/TimeEntryCollection.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace App\Http\Resources\V1\TimeEntry;
 
+use App\Http\Resources\PaginatedResourceCollection;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class TimeEntryCollection extends ResourceCollection
+class TimeEntryCollection extends ResourceCollection implements PaginatedResourceCollection
 {
     /**
      * The resource that this resource collects.
