fd77e1e9011f15c26af04bc2b51368b63724863a
Constantin Graf <constantin@solidtime.io>
2024-10-28 12:17:19 +0100

    Fix logo for email client with no SVG support like Gmail

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if(trim($slot) === 'solidtime')
-<img src="{{ asset('images/solidtime-logo.svg') }}" class="logo" alt="solidtime Logo">
+<img src="{{ asset('images/solidtime-logo.png') }}" srcset="{{ asset('images/solidtime-logo.svg') }}" class="logo" alt="solidtime Logo">
 @else
 {{ $slot }}
 @endif
