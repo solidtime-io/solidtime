b1b946654a97f868c47b679dc490a8455e1be830
Sergii Sadovyi <1175636+sergsadovyi@users.noreply.github.com>
2024-12-13 15:11:37 +0100

    [patch] use postmark env variables

--- config/services.php
+++ config/services.php
@@ -8,4 +8,7 @@ return [
         'basic_auth_username' => env('GOTENBERG_BASIC_AUTH_USERNAME'),
         'basic_auth_password' => env('GOTENBERG_BASIC_AUTH_PASSWORD'),
     ],
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
+    ],
 ];
