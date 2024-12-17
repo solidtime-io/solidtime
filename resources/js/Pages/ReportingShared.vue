6e226cd743b1734b6933ad31a040c1cd8fcbb8bb
Gregor Vostrak <gregor@solidtime.io>
2024-12-17 13:03:59 +0100

    hide report table for users that do not already have reports and cannot report new ones

--- resources/js/Pages/ReportingShared.vue
+++ resources/js/Pages/ReportingShared.vue
@@ -115,11 +115,11 @@ watch(currentPage, () => {
         </div>
 
         <ReportTable
-            v-if="reports"
-            :reports="reports"
-            show-billable-rate=""></ReportTable>
+            v-if="reports.length > 0 || isAllowedToPerformPremiumAction()"
+            :reports="reports"></ReportTable>
 
         <PaginationRoot
+            v-if="reports.length > 0 || isAllowedToPerformPremiumAction()"
             :total="totalPages"
             :items-per-page="pageLimit"
             class="flex justify-center items-center py-8"
