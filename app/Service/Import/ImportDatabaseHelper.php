84c9cfe2f2fef1075d7c74b13b942172b5e763dd
Constantin Graf <constantin@solidtime.io>
2025-02-04 19:51:54 -0500

    Fixed bugs causing incorrect computed attributes in imported data

--- app/Service/Import/ImportDatabaseHelper.php
+++ app/Service/Import/ImportDatabaseHelper.php
@@ -189,6 +189,17 @@ class ImportDatabaseHelper
     }
 
     /**
+     * @return array<TModel>
+     */
+    public function getCachedModels(): array
+    {
+        if ($this->mapKeyToModel === null) {
+            return [];
+        }
+        return array_values($this->mapKeyToModel);
+    }
+
+    /**
      * @param  array<string, mixed>  $identifierData
      * @return TModel|null
      */
