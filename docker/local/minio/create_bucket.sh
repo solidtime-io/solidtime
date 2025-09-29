0b45f3b473f5172b3ac068d99b5fb311e3de0044
Gregor Vostrak <gregor@solidtime.io>
2025-09-29 12:09:15 +0200

    change create bucket script to work with new minio client versions

--- docker/local/minio/create_bucket.sh
+++ docker/local/minio/create_bucket.sh
@@ -2,7 +2,7 @@
 
 # Source: https://helgesver.re/articles/laravel-sail-create-minio-bucket-automatically
 
-/usr/bin/mc config host add local ${S3_ENDPOINT} ${S3_ACCESS_KEY_ID} ${S3_SECRET_ACCESS_KEY};
+/usr/bin/mc alias set local ${S3_ENDPOINT} ${S3_ACCESS_KEY_ID} ${S3_SECRET_ACCESS_KEY};
 /usr/bin/mc rm -r --force local/${S3_BUCKET};
 /usr/bin/mc mb --ignore-existing local/${S3_BUCKET};
 /usr/bin/mc anonymous set public local/${S3_BUCKET};
