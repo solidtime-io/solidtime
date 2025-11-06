0648437478eba1fe4a9d651203b793e1bea4e556
Gregor Vostrak <gregor@solidtime.io>
2025-11-06 14:20:12 +0100

    design fixes, improve component encapsulation

--- resources/js/packages/ui/src/TimeEntry/TimeEntryRangeSelector.vue
+++ resources/js/packages/ui/src/TimeEntry/TimeEntryRangeSelector.vue
@@ -35,11 +35,11 @@ const organization = inject<ComputedRef<Organization>>('organization');
                     data-testid="time_entry_range_selector"
                     :class="
                         twMerge(
-                            'text-text-secondary px-2 bg-transparent text-center hover:bg-card-background rounded-lg border border-transparent hover:border-card-border focus-visible:outline-none focus:outline-none focus-visible:ring-2 focus-visible:text-text-primary focus-visible:ring-ring focus-visible:bg-tertiary',
+                            'text-text-secondary px-1 bg-transparent text-center hover:bg-card-background rounded-lg border border-transparent hover:border-card-border focus-visible:outline-none focus:outline-none focus-visible:ring-2 focus-visible:text-text-primary focus-visible:ring-ring focus-visible:bg-tertiary',
                             showDate
                                 ? 'text-xs py-1.5 font-semibold'
                                 : 'text-sm py-1.5 font-medium',
-                            organization?.time_format === '12-hours' ? 'w-[170px]' : 'w-[120px]',
+                            organization?.time_format === '12-hours' ? 'w-[150px]' : 'w-[90px]',
                             open && 'border-card-border bg-card-background'
                         )
                     ">
