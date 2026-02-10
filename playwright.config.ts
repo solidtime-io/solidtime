d06b0633d3e8af760fa0a7d287d9ff4a6cee36d4
Gregor Vostrak <gregor@solidtime.io>
2026-02-10 13:21:05 +0100

    add sharding for e2e tests in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     /* Run tests in parallel */
     workers: 4,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: process.env.CI ? 'line' : 'html',
+    reporter: process.env.CI ? 'blob' : 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
