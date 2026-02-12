6629482a0e53b83be8426f9cadc01f238e8f4f00
Gregor Vostrak <gregor@solidtime.io>
2026-02-12 18:12:05 +0100

    set maximum-scale=1 to prevent weird ios zoom behaviours

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
