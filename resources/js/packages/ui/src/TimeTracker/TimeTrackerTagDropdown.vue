cfbfbd4b6af1d5d0a31ebe3f676c42f3c9979ce7
Gregor Vostrak <gregor@solidtime.io>
2026-02-13 12:30:54 +0100

    remove no tags option from timetracker tag dropdown

--- resources/js/packages/ui/src/TimeTracker/TimeTrackerTagDropdown.vue
+++ resources/js/packages/ui/src/TimeTracker/TimeTrackerTagDropdown.vue
@@ -26,22 +26,22 @@ defineProps<{
 </script>
 
 <template>
-    <TagDropdown v-model="model" :create-tag :tags="tags" @changed="emit('changed')">
+    <TagDropdown
+        v-model="model"
+        :create-tag
+        :tags="tags"
+        :show-no-tag-option="false"
+        @changed="emit('changed')">
         <template #trigger>
             <button
                 data-testid="tag_dropdown"
                 :class="
                     twMerge(
                         iconColorClasses,
-                        'flex-shrink-0 ring-0 focus:outline-none focus:ring-2 focus:ring-ring transition focus-visible:bg-card-background-separator hover:bg-card-background-separator rounded-full w-10 h-10 flex items-center justify-center'
+                        'relative flex-shrink-0 ring-0 focus:outline-none focus:ring-2 focus:ring-ring transition focus-visible:bg-card-background-separator hover:bg-card-background-separator rounded-full w-10 h-10 flex items-center justify-center'
                     )
                 ">
                 <TagIcon class="w-5 h-5 lg:h-6 lg:w-6"></TagIcon>
-                <span
-                    v-if="model.length > 1"
-                    class="font-extrabold absolute rounded-full text-xs w-3 h-3 block top-[15px] rotate-[45deg] right-[14px] text-card-background">
-                    {{ model.length }}
-                </span>
             </button>
         </template>
     </TagDropdown>
