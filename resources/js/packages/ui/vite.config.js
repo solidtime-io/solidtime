b906ca151104b0b6151e97392aa35dfecf60c94e
Gregor Vostrak <gregor@solidtime.io>
2026-03-05 15:18:35 +0100

    add lucide-vue-next to peer dependencies

--- resources/js/packages/ui/vite.config.js
+++ resources/js/packages/ui/vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
         rollupOptions: {
             // make sure to externalize deps that shouldn't be bundled
             // into your library
-            external: ['vue'],
+            external: ['vue', 'lucide-vue-next'],
             output: {
                 // Provide global variables to use in the UMD build
                 // for externalized deps
