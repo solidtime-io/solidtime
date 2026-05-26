400a76466303015cece738aedc1af762d2538d52
Gregor Vostrak <gregor@solidtime.io>
2026-05-26 11:05:51 +0200

    improve modal and field group spacing for project modal layout

--- resources/js/packages/ui/src/field/FieldGroup.vue
+++ resources/js/packages/ui/src/field/FieldGroup.vue
@@ -12,7 +12,7 @@ const props = defineProps<{
         data-slot="field-group"
         :class="
             cn(
-                'group/field-group @container/field-group flex w-full flex-col gap-7 data-[slot=checkbox-group]:gap-3 [&>[data-slot=field-group]]:gap-4',
+                'group/field-group @container/field-group flex w-full flex-col gap-6 data-[slot=checkbox-group]:gap-3 [&>[data-slot=field-group]]:gap-4',
                 props.class
             )
         ">
