a050153bcd56e377d1a8354438b02be11a282cfe
Darwin Correa <dacopan@users.noreply.github.com>
2026-08-20 10:22:24 -0500

    feat: make API rate limits configurable via env vars (#1204)
    * feat: make API rate limits configurable via env vars

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -33,8 +33,8 @@ class RouteServiceProvider extends ServiceProvider
             }
 
             return $request->user()
-                ? Limit::perMinute(200)->by($request->user()->id)
-                : Limit::perMinute(60)->by($request->ip());
+                ? Limit::perMinute(config('services.api.authenticated'))->by($request->user()->id)
+                : Limit::perMinute(config('services.api.guest'))->by($request->ip());
         });
 
         $this->routes(function (): void {
